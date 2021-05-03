8d4aaa2d97f3ebc3442c2129685043b51afa80a1
weedskimhj <kimhj@weedscomm.com>
2021-05-03 10:32:56 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://joo:joo123@cluster0.qf67u.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => {
+    console.log('http://localhost:3000');
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
